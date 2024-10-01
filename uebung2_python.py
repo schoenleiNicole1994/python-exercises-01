a19b7bf8e48c605419a1889747885393af83245a
nicole <nicole07041994@gmail.com>
2024-10-01 13:50:41 +0200

    add uebung2_python.py

---
+++ uebung2_python.py
@@ -0,0 +1,19 @@
+# Definiere die Ganzzahlen
+a = 10
+b = 3
+
+# Berechne die verschiedenen Werte
+summe = a + b
+differenz = a - b
+produkt = a * b
+ganzzahliger_quotient = a // b
+rest = a % b
+hoch = a ** b
+
+# Gib die Ergebnisse auf der Konsole aus
+print("Summe:", summe)
+print("Differenz:", differenz)
+print("Produkt:", produkt)
+print("Ganzzahliger Quotient:", ganzzahliger_quotient)
+print("Rest der Division:", rest)
+print("a hoch b:", hoch)
