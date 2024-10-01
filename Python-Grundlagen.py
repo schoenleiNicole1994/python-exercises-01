5a087b56242195c2797ce4ee5a567d460b3a7b04
nicole <nicole07041994@gmail.com>
2024-10-01 13:45:41 +0200

    add Python-Grundlagen.py

---
+++ Python-Grundlagen.py
@@ -0,0 +1,11 @@
+# Definiere die Variablen
+first_name = "Nicole"    #string
+last_name = "schoenlein" #string
+birth_year = 1994  # Beispieljahr
+has_license = False  # boolean (true oder False)
+
+# Gib die Variablenwerte auf der Konsole aus
+print("Vorname:", first_name)
+print("Nachname:", last_name)
+print("Geburtsjahr:", birth_year)
+print("Führerschein:", has_license)
