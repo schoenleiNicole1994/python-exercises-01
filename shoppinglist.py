73ff13d448ba341004b3f8ce877f836eb772b8e7
nicole <nicole07041994@gmail.com>
2024-10-02 13:19:19 +0200

    Add function to show the shopping list

--- shoppinglist.py
+++ shoppinglist.py
@@ -3,3 +3,11 @@ def add_item():
     item = input("Bitte gib den Artikel ein, der zur Einkaufsliste hinzugefügt werden soll: ")
     shoppinglist.append(item)
     print(f"{item} wurde der Einkaufsliste hinzugefügt.")
+    
+    def show_shoppinglist():
+    if shoppinglist:
+        print("Deine Einkaufsliste:")
+        for item in shoppinglist:
+            print(f"- {item}")
+    else:
+        print("Deine Einkaufsliste ist leer.")
